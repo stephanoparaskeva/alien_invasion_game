6f2405bcc2ff9c49d1f10efc13eaddd2e0f00248
Stephano Paraskeva <stephanokparaskeva@gmail.com>
2019-01-26 02:57:26 +0000

    minor changes to settings

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship():
         self.screen = screen
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
@@ -15,6 +15,6 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        def blitme(self):
-            """Draw the ship at its current location."""
-            self.screen.blit(self.image, self.rect)
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
